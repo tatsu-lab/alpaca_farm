7bc828a3624de56a6588329a020f910005ce9ab8
Xuechen Li <lxuechen@gmail.com>
2023-05-14 20:14:01 -0700

    fix trainer.

--- examples/reward_modeling.py
+++ examples/reward_modeling.py
@@ -1,15 +1,11 @@
 import contextlib
 import os
 from dataclasses import dataclass, field
-from typing import Dict, List, Literal, Optional
+from typing import List, Literal, Optional
 
-import einops
-import torch
-import torch.nn.functional as F
 import transformers
-from transformers.trainer_utils import EvalPrediction
 
-from alpaca_farm import common, constants, torch_ops, data_postprocessor
+from alpaca_farm import common, constants, data_postprocessor, trainer_reward_modeling
 from alpaca_farm.models import reward_model
 
 
@@ -85,50 +81,6 @@ class TrainingArguments(transformers.TrainingArguments):
     )
 
 
-class Trainer(transformers.Trainer):
-    def compute_loss(self, model, inputs, return_outputs=False):
-        # input_ids, attention_mask each of size (bsz, num_candidates, seq_len).
-        # index_0, index_1 each of size (bsz, num_pairs); indexes into input_ids.
-        # choice of size (bsz, num_pairs); 1 if index_1's seq is chosen, 0 otherwise.
-        input_ids, attention_mask, index_0, index_1, choice = common.unpack_dict(
-            inputs, keys=("input_ids", "attention_mask", "index_0", "index_1", "choice")
-        )
-        num_candidates, num_pairs = input_ids.size(1), choice.size(1)
-        input_ids_flat, attention_mask_flat = tuple(
-            einops.rearrange(x, "b c l -> (b c) l") for x in (input_ids, attention_mask)
-        )
-        outputs = model(input_ids=input_ids_flat, attention_mask=attention_mask_flat)
-        rewards_flat = outputs.rewards
-        rewards = einops.rearrange(rewards_flat, "(b c) -> b c", c=num_candidates)  # Size: (bsz, num_candidates).
-
-        rewards_0, rewards_1 = tuple(
-            torch_ops.batch_select(rewards, index) for index in (index_0, index_1)
-        )  # Size: (bsz, num_pairs).
-        logits = rewards_1 - rewards_0  # Size: (bsz, num_pairs).
-        # Type casting of `choice` is due to amp.autocast context manager.
-        loss = F.binary_cross_entropy_with_logits(logits, choice.to(logits.dtype), reduction="mean")
-        return (loss, dict(logits=logits)) if return_outputs else loss
-
-
-def compute_reward_modeling_metrics(eval_prediction: EvalPrediction) -> Dict:
-    # eval_prediction.label_ids is a tuple that matches up with `training_args.label_names`.
-    logits = torch.tensor(eval_prediction.predictions).squeeze(-1)
-    labels = torch.tensor(eval_prediction.label_ids[-1]).squeeze(-1)
-    predictions = (logits >= 0.0).long()
-    accuracy = predictions.eq(labels).float().mean().item()
-    label_positive_rate = (labels == 1).float().mean().item()
-    positive_rate = (predictions == 1).float().mean().item()
-    true_positive_rate = (predictions * labels).float().sum().item() / labels.sum().item()
-    false_positive_rate = (predictions * (1 - labels)).float().sum().item() / (1 - labels).sum().item()
-    return dict(
-        accuracy=accuracy,
-        label_positive_rate=label_positive_rate,
-        positive_rate=positive_rate,
-        true_positive_rate=true_positive_rate,
-        false_positive_rate=false_positive_rate,
-    )
-
-
 def main():
     parser = transformers.HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
@@ -174,11 +126,11 @@ def main():
         training_args=training_args,
     )
 
-    trainer = Trainer(
+    trainer = trainer_reward_modeling.Trainer(
         model=model,
         tokenizer=tokenizer,
         args=training_args,
-        compute_metrics=compute_reward_modeling_metrics,
+        compute_metrics=trainer_reward_modeling.compute_reward_modeling_metrics,
         **data_module,
     )
 
