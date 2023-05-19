e0b653589205da8d3b6e6ebde3fb3a49a9ea8a31
Xuechen Li <lxuechen@gmail.com>
2023-05-19 12:57:20 -0700

    refac decode.

--- examples/best_of_n.py
+++ examples/best_of_n.py
@@ -1,38 +1,44 @@
 import pathlib
 import sys
+from typing import Optional
 
 import datasets
 import fire
 
-from alpaca_farm import data_preprocessor, utils
+from alpaca_farm import common, data_preprocessor, utils
 from alpaca_farm.inference import decode
+from alpaca_farm.types import AnyPathOrNone
 
 
 def run_decode(
     model_name_or_path,
     prompt_dict_path=pathlib.Path(__file__).parent / "prompts" / "v0_inputs_noinputs.json",
-    use_auth_token=None,
+    output_path: AnyPathOrNone = None,
+    use_auth_token: Optional[str] = None,
     split="val",
     max_instances=sys.maxsize,
 ):
     dataset = datasets.load_dataset("tatsu-lab/alpaca_farm", "alpaca_instructions", use_auth_token=use_auth_token)
-    prompts = data_preprocessor.format_prompt_with_huggingface_dataset(
+    prompts, list_dict_data, metadata = data_preprocessor.format_prompt_with_huggingface_dataset(
         huggingface_dataset=dataset[split],
         prompt_dict=utils.jload(prompt_dict_path),
-        return_dict=False,
     )
 
+    prompts, list_dict_data = prompts[:max_instances], list_dict_data[:max_instances]
     completions = decode.decode_prompts_with_huggingface(
         model_name_or_path,
         prompts=prompts,
         decoding_args=decode.HFDecodingArguments(temperature=0.7, max_new_tokens=300),
-        max_instances=max_instances,
     )
-    print(prompts[:max_instances])
-    print(completions)
+
+    model_name = common.get_pretrained_model_name_with_model_name_or_path(model_name_or_path)
+    dump_list_dict_data = [
+        {**dict_data, model_name: completion} for dict_data, completion in utils.zip_(list_dict_data, completions)
+    ]
+    utils.jdump(dump_list_dict_data, output_path)
 
 
-def run_score():
+def run_rerank():
     pass
 
 
