32030c043cdaeabf9a7d6fad55eca260a8577814
Xuechen Li <lxuechen@gmail.com>
2023-05-19 13:16:45 -0700

    rerank.

--- examples/best_of_n.py
+++ examples/best_of_n.py
@@ -5,13 +5,13 @@ from typing import Optional
 import datasets
 import fire
 
-from alpaca_farm import common, data_preprocessor, utils
+from alpaca_farm import data_preprocessor, utils
 from alpaca_farm.inference import decode, score
 from alpaca_farm.types import AnyPath, AnyPathOrNone
 
 
 def run_decode(
-    model_name_or_path,
+    decoder_name_or_path: AnyPath,
     prompt_dict_path=pathlib.Path(__file__).parent / "prompts" / "v0_inputs_noinputs.json",
     output_path: AnyPathOrNone = None,
     use_auth_token: Optional[str] = None,
@@ -22,6 +22,7 @@ def run_decode(
     max_new_tokens=300,
 ):
     dataset = datasets.load_dataset("tatsu-lab/alpaca_farm", "alpaca_instructions", use_auth_token=use_auth_token)
+
     prompts, list_dict_data, metadata = data_preprocessor.format_prompt_with_huggingface_dataset(
         huggingface_dataset=dataset[split],
         prompt_dict=utils.jload(prompt_dict_path),
@@ -29,7 +30,7 @@ def run_decode(
     prompts, list_dict_data = prompts[:max_instances], list_dict_data[:max_instances]
 
     completions = decode.decode_prompts_with_huggingface(
-        model_name_or_path=model_name_or_path,
+        model_name_or_path=decoder_name_or_path,
         prompts=prompts,
         decoding_args=decode.HFDecodingArguments(
             temperature=temperature, max_new_tokens=max_new_tokens, num_return_sequences=num_return_sequences
@@ -37,23 +38,49 @@ def run_decode(
     )
 
     return_list_dict_data = [
-        {**dict_data, "prompt": prompt, "completion": completion}
+        {"prompt": prompt, "completion": completion, "decoder_name_or_path": decoder_name_or_path}
         for dict_data, prompt, completion in utils.zip_(list_dict_data, prompts, completions)
     ]
-    utils.jdump(return_list_dict_data, output_path)
+    if output_path is not None:
+        utils.jdump(return_list_dict_data, output_path)
+
     return return_list_dict_data
 
 
 def run_rerank(
     list_dict_data_or_path,
-    model_name_or_path,  # Reward model path or name.
+    scorer_name_or_path: AnyPath,
+    output_path: AnyPathOrNone = None,
 ):
     if isinstance(list_dict_data_or_path, AnyPath):
-        pass
-    score.rerank_sequences_with_huggingface(sequences, model_name_or_path)
+        list_dict_data_or_path = utils.jload(list_dict_data_or_path)
+
+    sequences = [dict_data["prompt"] + dict_data["completion"] for dict_data in list_dict_data_or_path]
+
+    top_sequences, top_indices = score.rerank_sequences_with_huggingface(sequences, scorer_name_or_path)
+
+    return_list_dict_data = [
+        {"top_sequence": top_sequence, "top_index": top_index, "scorer_name_or_path": scorer_name_or_path}
+        for top_sequence, top_index in utils.zip_(top_sequences, top_indices)
+    ]
+    if output_path is not None:
+        utils.jdump(return_list_dict_data, output_path)
+
+    return return_list_dict_data
 
 
-def run_best_of_n():
+def run_best_of_n(
+    decoder_name_or_path,
+    scorer_name_or_path,
+    prompt_dict_path=pathlib.Path(__file__).parent / "prompts" / "v0_inputs_noinputs.json",
+    output_path: AnyPathOrNone = None,
+    use_auth_token: Optional[str] = None,
+    split="val",
+    max_instances=sys.maxsize,
+    temperature=1.0,
+    num_return_sequences=4,
+    max_new_tokens=300,
+):
     """Chain together decoding and rerank."""
     pass
 
