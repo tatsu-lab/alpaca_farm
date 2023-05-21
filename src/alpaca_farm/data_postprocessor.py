557de2dc654e904cd27fa51eb07a12642e972e21
Xuechen Li <lxuechen@gmail.com>
2023-05-21 15:16:16 -0700

    patch license.

--- src/alpaca_farm/data_postprocessor.py
+++ src/alpaca_farm/data_postprocessor.py
@@ -1,5 +1,23 @@
-# Stuff that reformats prompts (RC, Quark) goes here.
-# maps to postprocessors.py
+# Copyright 2023 The Alpaca Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Postprocessors for prompts and data frames.
+
+Internal map:
+    https://github.com/lxuechen/human-feedback/blob/main/instruction_following/postprocessor.py
+"""
 
 from dataclasses import dataclass
 from typing import Callable, Sequence, Union
