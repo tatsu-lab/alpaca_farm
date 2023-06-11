e0928400538511f6c7f0d713007141cdb5e85179
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-06-11 14:22:37 -0700

    improve doc.

--- examples/supervised.py
+++ examples/supervised.py
@@ -16,7 +16,7 @@ import contextlib
 import os
 import pathlib
 from dataclasses import dataclass, field
-from typing import List, Literal
+from typing import List, Literal, Optional
 
 import transformers
 from transformers import Trainer
@@ -35,10 +35,29 @@ class ModelArguments:
 
 @dataclass
 class DataArguments:
-    dataset_path: str = field(default="tatsu-lab/alpaca_farm")
-    dataset_name: str = field(default="alpaca_instructions")
-    train_splits: List[str] = field(default_factory=lambda: ["sft"])
-    eval_splits: List[str] = field(default_factory=lambda: ["val"])
+    dataset_path: str = field(
+        default="tatsu-lab/alpaca_farm",
+        metadata={
+            "help": "Path to the dataset. Either points to a location on Hugging Face hub or a local folder. "
+                    "If the path points to a local folder, the folder must be structured properly "
+                    "(see documentation for datasets.load_dataset)."
+        }
+    )
+    dataset_name: Optional[str] = field(
+        default="alpaca_instructions",
+        metadata={"help": "Name of the dataset to load -- the argument `name` passed to `datasets.load_dataset`."},
+    )
+    train_splits: List[str] = field(
+        default_factory=lambda: ["sft"],
+        metadata={"help": "Splits to use for training. This must not be an empty list."}
+    )
+    eval_splits: Optional[List[str]] = field(
+        default_factory=lambda: ["val"],
+        metadata={
+            "help": "Splits to use for evaluation. "
+                    "If None, empty, or the splits are not found in the dataset, no evaluation is performed."
+        },
+    )
     prompt_dict_path: str = field(
         default=pathlib.Path(__file__).parent / "prompts" / "v0_inputs_noinputs.json",
         metadata={"help": "Path to the dictionary for the prompt to format examples."},
@@ -56,21 +75,21 @@ class TrainingArguments(transformers.TrainingArguments):
         default=512,
         metadata={
             "help": "Maximum sequence length. Sequences will be right padded to this length (and possibly truncated)."
-            "Enforcing a consistent max length ensures memory usage is constant and predictable."
+                    "Enforcing a consistent max length ensures memory usage is constant and predictable."
         },
     )
     padding: Literal["max_length", "longest"] = field(
         default="longest",
         metadata={
             "help": "Padding strategy. If 'max_length', pads to `model_max_length` always; this might lead to some "
-            "redundant compute. If 'longest', pads to the longest sequence in the batch, capped by `model_max_length`."
+                    "redundant compute. If 'longest', pads to the longest sequence in the batch, capped by `model_max_length`."
         },
     )
     initialize_model_on_cpu: bool = field(
         default=False,
         metadata={
             "help": "Whether to initialize the model on CPU. "
-            "If True, models on all processes will be first initialized on CPU; this is RAM-costly but faster."
+                    "If True, models on all processes will be first initialized on CPU; this is RAM-costly but faster."
         },
     )
     resume_from_checkpoint: bool = field(default=False, metadata={"help": "If True, loads from last check point."})
@@ -78,8 +97,8 @@ class TrainingArguments(transformers.TrainingArguments):
         default=False,
         metadata={
             "help": "Use fast tokenizer if True. "
-            "Fast LLaMA tokenizer forces protobuf downgrade to 3.20.3. "
-            "Use fast tokenizer only if you can live with that."
+                    "Fast LLaMA tokenizer forces protobuf downgrade to 3.20.3. "
+                    "Use fast tokenizer only if you can live with that."
         },
     )
 
