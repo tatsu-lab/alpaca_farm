49f3976c5507a3dcf2236fe1d6057f42aa8f7f35
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-06-11 13:31:10 -0700

    bump version.

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2023 The Alpaca Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import re
 
@@ -65,4 +79,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
 )
