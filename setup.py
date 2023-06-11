61b2c7393b08e745ac235bcd433af9afca281a9d
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-06-10 20:40:04 -0700

    add package data.

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setuptools.setup(
     version=version,
     package_dir={"": "src"},
     packages=setuptools.find_packages("src"),
+    package_data={"": ["alpaca_farm/auto_annotations/annotators/*/*.yaml"]},
+    include_package_data=True,
     install_requires=[
         "datasets",
         "einops",
