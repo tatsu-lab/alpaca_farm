050b370b189254df8de9c2f1c14b51cdf48dc326
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-06-14 21:30:15 -0700

    fix import issues.

--- src/alpaca_farm/auto_annotations/__init__.py
+++ src/alpaca_farm/auto_annotations/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .eval import *
-from .pairwise_annotators import *
+from .eval import alpaca_leaderboard
+from .pairwise_annotators import PairwiseAutoAnnotator
