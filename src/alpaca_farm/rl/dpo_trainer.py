a57411c65a91596d12f69066c2af8ee8cfbe664e
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-11-23 15:56:48 -0800

    chore: doc efficient impl clarify

--- src/alpaca_farm/rl/dpo_trainer.py
+++ src/alpaca_farm/rl/dpo_trainer.py
@@ -19,13 +19,15 @@ class Trainer(transformers.Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         # TODO: This implementation is simple and readable, but it's not efficient.
         #  Since the instruction+input is shared for the winning and losing sequences, one can in principle
-        #  just do a single forward pass on this part for each model, instead of doing the full forward
-        #  twice (one for winning and one for losing sequence).
+        #  just do a single forward pass on this part for model and ref_model, instead of doing the full forward
+        #  twice (one for winning and one for losing sequence) for model and ref_model.
         #  So here's the efficient implementation:
-        #   1. Do a single forward pass on the instruction+input for policy. Retain the kv cache.
+        #   1. Do a single forward pass on the instruction+input for model. Retain the kv cache.
         #   2. Do a forward pass on the winning response for model, using the kv cache.
         #   3. Do a forward pass on the losing response for model, using the kv cache.
-        #   4. Follow a similar procedure for the reference model, except don't need to retain activations for backprop.
+        #   4. Follow a similar procedure for ref_model, except don't retain activations for backprop
+        #       (but do temporarily retain the kv cache).
+        #       There's an explicit speed/memory tradeoff here -- retaining kv cache saves FLOPs but uses more memory.
         #   5. Compute the loss.
         input_ids_w, labels_w, attention_mask_w, input_ids_l, labels_l, attention_mask_l = common.unpack_dict(
             inputs, LABEL_NAMES
