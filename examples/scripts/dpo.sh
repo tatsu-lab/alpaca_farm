49c44c30d75611dfbb49ec2cf55095303449fbea
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-12-05 09:31:31 -0800

    fix: standardize hparam for dpo

--- examples/scripts/dpo.sh
+++ examples/scripts/dpo.sh
@@ -13,12 +13,13 @@ torchrun --nproc_per_node=8 --master_port=1234 examples/dpo.py \
   --num_train_epochs 2 \
   --per_device_train_batch_size 1 \
   --per_device_eval_batch_size 4 \
-  --gradient_accumulation_steps 16 \
+  --gradient_accumulation_steps 8 \
   --eval_steps 100 \
   --save_strategy "steps" \
   --save_steps 1000000000 \
   --save_total_limit 1 \
   --learning_rate 1e-6 \
+  --beta 0.1 \
   --weight_decay 0.0 \
   --warmup_ratio 0.03 \
   --lr_scheduler_type "cosine" \
